41f3f5d1cfe4fe52a12a1b3beeb13460e8678782
Ankush kumar <ankushkumarsingh374@gmail.com>
2025-07-19 22:58:09 +0530

    application status updated feature added

--- FRONTEND/src/components/admin/ApplicantsTable.jsx
+++ FRONTEND/src/components/admin/ApplicantsTable.jsx
@@ -11,9 +11,27 @@ import {
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { MoreHorizontal } from "lucide-react";
 import { useSelector } from "react-redux";
+import { toast } from "sonner";
+import axios from "axios";
+import { APPLICATION_API_END_POINT } from "@/utils/Constant";
 const shortlistingStatus = ["Accepted", "Rejected"];
 const ApplicantsTable = () => {
   const { applicants } = useSelector((store) => store.application);
+  const statusHandler = async (status, id) => {
+    try {
+      axios.defaults.withCredentials=true;
+      const res = await axios.post(
+        `${APPLICATION_API_END_POINT}/status/${id}/update`,
+        { status },
+        { withCredentials: true }
+      );
+      if(res.data.success){
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
   return (
     <div>
       <Table>
@@ -35,9 +53,23 @@ const ApplicantsTable = () => {
                 <TableCell>{item?.applicant?.fullname}</TableCell>
                 <TableCell>{item?.applicant?.email}</TableCell>
                 <TableCell>{item?.applicant?.phonenumber}</TableCell>
-                <TableCell >{item.applicant?.profile?.resume ? <a className="text-blue-600 cursor-pointer" href={item?.applicant?.profile?.resume} target="_blank" rel="noopener noreferrer">{item?.applicant?.profile?.resumeOriginalName}</a> :
-                <span>NA</span> }</TableCell>
-                <TableCell>{item?.applicant?.createdAt.split("T")[0]}</TableCell>
+                <TableCell>
+                  {item.applicant?.profile?.resume ? (
+                    <a
+                      className="text-blue-600 cursor-pointer"
+                      href={item?.applicant?.profile?.resume}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item?.applicant?.profile?.resumeOriginalName}
+                    </a>
+                  ) : (
+                    <span>NA</span>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {item?.applicant?.createdAt.split("T")[0]}
+                </TableCell>
                 <TableCell className="float-right cursor-pointer">
                   <Popover>
                     <PopoverTrigger>
@@ -46,7 +78,7 @@ const ApplicantsTable = () => {
                     <PopoverContent className="w-32">
                       {shortlistingStatus.map((status, index) => {
                         return (
-                          <div key={index}>
+                          <div  onClick={()=>statusHandler(status,item?._id)} key={index}>
                             <span>{status}</span>
                           </div>
                         );
